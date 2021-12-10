84f0dfc02c38b605d66c9f4950e96a08ffd58f6e
unknown <pavelstaheev1@gmail.com>
2021-12-10 15:06:52 +0200

    Removed telegraf package, app refactored using express, ngrok, axios

--- app.js
+++ app.js
@@ -1,167 +1,217 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import fetch from "node-fetch";
-import { requestOptions } from "./src/cryptoApiParam.js";
+import axios from "axios";
 import { userModel } from "./src/cryptoSchema.js";
-import { Telegraf } from "telegraf";
+import express from "express";
 
 dotenv.config();
+mongoose.connect(process.env.DATABASE_URL);
+const telegram_url = `https://api.telegram.org/bot${process.env.TELEGRAM}/`;
+const app = express();
+app.use(express.json());
 
-const bot = new Telegraf(process.env.TELEGRAM);
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+function main() {
+  let symbolArr, deleteCryptoArr, addCryptoArr;
 
-async function app() {
-  const { data } = await fetch(
-    process.env.COIN_MARKET_URL,
-    requestOptions
-  ).then((res) => res.json());
+  app.post("/", async (req, res) => {
+    const chatId =
+      req.body.message?.chat.id || req.body.callback_query.message.chat.id;
+    const sendMessage = req.body.message?.text;
+    const userName = req.body.message?.from.username;
 
-  const cryptoArr = data.slice(0, 20).map((crypto) => ({
-    name: crypto.name,
-    symbol: crypto.symbol,
-    amountInUse: crypto.circulating_supply,
-    maxAmount: crypto.max_supply,
-    quote: crypto.quote.USD,
-  }));
+    if (sendMessage === "/start") {
+      const user = new userModel({
+        userName,
+        cryptos: [],
+      });
 
-  const curSymArr = cryptoArr.map((cur) => cur.symbol);
-  const curNameArr = cryptoArr.map((cur) => cur.name);
+      userModel.exists({ userName }).then((res) => res || user.save());
 
-  bot.start((ctx) => {
-    const user = new userModel({
-      userName: ctx.from.username,
-      cryptos: [],
-    });
-    userModel
-      .exists({ userName: user.userName })
-      .then((res) => res || user.save());
+      const text = `Hello, ${
+        req.body.message.from.first_name || "my friend"
+      }! Type /help to see what I can!`;
 
-    ctx.reply(
-      `Hello, ${
-        ctx.from.first_name || "my friend"
-      }! Type /help to see what I can!`
-    );
-  });
+      sendAnswer(text, chatId, res);
+    } else if (sendMessage === "/help") {
+      const text =
+        "I'm a crypto bot!! I can provide you with crypto information! I support such commands:\n/listRecent\n/listFavorite\n/addToFavorite {currency name}\n/deleteFavorite {currency name}";
+      sendAnswer(text, chatId, res);
+    } else if (sendMessage === "/listRecent") {
+      const data = await axios
+        .get(process.env.COIN_MARKET_URL, {
+          params: {
+            limit: 20,
+          },
+          headers: {
+            "X-CMC_PRO_API_KEY": process.env.COIN_KEY,
+          },
+        })
+        .then((res) => res.data.data)
+        .catch((err) => console.log(err.message));
 
-  bot.help((ctx) =>
-    ctx.reply(
-      "I'm a crypto bot!! I can provide you with crypto information! I support such commands: /listRecent, /listFavorite, /addToFavorite {currency name}, /deleteFavorite {currency name}"
-    )
-  );
-
-  bot.command("listRecent", async (ctx) => {
-    const resStr = [];
-
-    cryptoArr.forEach(async (crypto) =>
-      resStr.push(`/${crypto.symbol} $${crypto.quote.price.toFixed(2)}`)
-    );
-    ctx.reply(`List of crypto currecies: 
-${resStr.join("\n")}`);
-  });
-
-  curSymArr.forEach((symbol) => {
-    bot.command(`${symbol}`, (ctx) => {
-      displayCryptoInfo(
-        ctx,
-        cryptoArr.find((cur) => cur.symbol === symbol)
+      symbolArr = data.map((crypto) => `/${crypto.symbol}`);
+      deleteCryptoArr = data.map(
+        (crypto) => `/deleteFavorite ${crypto.symbol}`
       );
-    });
+      addCryptoArr = data.map((crypto) => `/addToFavorite ${crypto.symbol}`);
+      const text = data
+        .map(
+          (crypto) => `/${crypto.symbol} $${crypto.quote.USD.price.toFixed(2)}`
+        )
+        .join("\n");
+      sendAnswer(text, chatId, res);
+    } else if (symbolArr.includes(sendMessage)) {
+      const userObj = await userModel.findOne({
+        userName,
+      });
+      const symbol = sendMessage.slice(1);
+      const cryptoData = await axios
+        .get(process.env.COIN_MARKET_SINGLE, {
+          params: {
+            symbol,
+          },
+          headers: {
+            "X-CMC_PRO_API_KEY": process.env.COIN_KEY,
+          },
+        })
+        .then((res) => res.data.data)
+        .catch((err) => console.log(err.message));
 
-    bot.action(`${symbol}`, async (ctx) => {
-      ctx.deleteMessage();
-      const currentObj = cryptoArr.find((cur) => cur.symbol === symbol);
-      followCurrency(ctx.from.username, currentObj.name);
-      ctx.reply(`${currentObj.name} is processed!`);
-    });
-  });
+      const text = currencyString(cryptoData[symbol]);
+      axios
+        .post(`${telegram_url}sendMessage`, {
+          chat_id: chatId,
+          text,
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: userObj.cryptos.includes(symbol)
+                    ? "Remove from favorite"
+                    : "Add to favorite",
+                  callback_data: symbol,
+                },
+              ],
+            ],
+          },
+        })
+        .then((response) => res.status(200).send(response))
+        .catch((err) => res.send(err));
+    } else if (req.body.callback_query) {
+      const userName = req.body.callback_query.from.username;
+      const cryptoSymbol = req.body.callback_query.data;
+      const text = `${cryptoSymbol} is processed!`;
+      const { cryptos } = await userModel.findOne({
+        userName,
+      });
 
-  bot.command("listFavorite", async (ctx) => {
-    const { cryptos } = await userModel.findOne({
-      userName: ctx.from.username,
-    });
+      cryptos.includes(cryptoSymbol)
+        ? cryptos.splice(cryptos.indexOf(cryptoSymbol), 1)
+        : cryptos.push(cryptoSymbol);
 
-    const followingCryptos = cryptoArr.filter((cur) =>
-      cryptos.includes(cur.name)
-    );
+      await userModel.findOneAndUpdate({ userName }, { cryptos });
+      axios
+        .post(`${telegram_url}editMessageText`, {
+          chat_id: chatId,
+          text,
+          message_id: req.body.callback_query.message.message_id,
+        })
+        .then((response) => res.status(200).send(response))
+        .catch((err) => res.send(err));
+    } else if (deleteCryptoArr.includes(sendMessage)) {
+      const cryptoSymbol = sendMessage.slice(sendMessage.indexOf(" ") + 1);
+      const { cryptos } = await userModel.findOne({
+        userName,
+      });
 
-    const resStr = followingCryptos.map(
-      (cur) => `/${cur.symbol} $${cur.quote.price.toFixed(2)}`
-    );
-    if (resStr.length === 0) ctx.reply("No currency in favorite!");
-    else {
-      ctx.reply(`Your favorite list:
-${resStr.join("\n")}`);
+      const text = cryptos.includes(cryptoSymbol)
+        ? `${cryptoSymbol} deleted from favorite!`
+        : `${cryptoSymbol} is not in favorite.`;
+
+      cryptos.includes(cryptoSymbol) &&
+        cryptos.splice(cryptos.indexOf(cryptoSymbol), 1);
+
+      await userModel.findOneAndUpdate({ userName }, { cryptos });
+
+      sendAnswer(text, chatId, res);
+    } else if (addCryptoArr.includes(sendMessage)) {
+      const cryptoSymbol = sendMessage.slice(sendMessage.indexOf(" ") + 1);
+      const { cryptos } = await userModel.findOne({
+        userName,
+      });
+
+      const text = !cryptos.includes(cryptoSymbol)
+        ? `${cryptoSymbol} added to favorite!`
+        : `${cryptoSymbol} is already in favorite.`;
+
+      cryptos.includes(cryptoSymbol) || cryptos.push(cryptoSymbol);
+
+      await userModel.findOneAndUpdate({ userName }, { cryptos });
+      sendAnswer(text, chatId, res);
+      return;
+    } else if (
+      sendMessage === "/deleteFavorite" ||
+      sendMessage === "/addToFavorite"
+    ) {
+      const text = "You should enter currency name!";
+      sendAnswer(text, chatId, res);
+      return;
+    } else if (
+      sendMessage.startsWith("/deleteFavorite") ||
+      sendMessage.startsWith("/addToFavorite")
+    ) {
+      const text = "Invalid currency name.";
+      sendAnswer(text, chatId, res);
+      return;
+    } else if (sendMessage === "/listFavorite") {
+      const { cryptos } = await userModel.findOne({
+        userName,
+      });
+
+      const followingCryptos = await axios
+        .get(process.env.COIN_MARKET_SINGLE, {
+          params: {
+            symbol: cryptos.toString(),
+          },
+          headers: {
+            "X-CMC_PRO_API_KEY": process.env.COIN_KEY,
+          },
+        })
+        .then((res) => res.data.data)
+        .catch((err) => console.log(err));
+
+      const responseArr = cryptos.map(
+        (crypto) =>
+          `/${followingCryptos[crypto].symbol} $${followingCryptos[
+            crypto
+          ].quote.USD.price.toFixed(2)}`
+      );
+
+      const text =
+        responseArr.length === 0
+          ? "No currency in favorite!"
+          : `Your favorite list:\n${responseArr.join("\n")}`;
+      sendAnswer(text, chatId, res);
+    } else {
+      const text = "I didn't understand you, could you repeat, please?";
+      sendAnswer(text, chatId, res);
     }
   });
-
-  bot.on("message", (ctx) => {
-    if (
-      ctx.update.message.text === "/addToFavorite" ||
-      ctx.update.message.text === "/deleteFavorite"
-    )
-      ctx.reply("You should enter currency name!");
-    else if (
-      !ctx.update.message.text.includes("/addToFavorite") &&
-      !ctx.update.message.text.includes("/deleteFavorite")
-    )
-      ctx.reply("I didn’t understand you, could you repeat, please?");
-    else
-      curNameArr.forEach(async (cur) => {
-        if (ctx.update.message.text === `/addToFavorite ${cur}`) {
-          const { cryptos } = await userModel.findOne({
-            userName: ctx.from.username,
-          });
-
-          if (!cryptos.includes(cur)) {
-            cryptos.push(cur);
-            await userModel.findOneAndUpdate(
-              { userName: ctx.from.username },
-              { cryptos }
-            );
-            ctx.reply(`${cur} added to favorite!`);
-          } else ctx.reply(`${cur} is already in favorite.`);
-        } else if (ctx.update.message.text === `/deleteFavorite ${cur}`) {
-          const { cryptos } = await userModel.findOne({
-            userName: ctx.from.username,
-          });
-
-          if (cryptos.includes(cur)) {
-            cryptos.splice(cryptos.indexOf(cur), 1);
-            await userModel.findOneAndUpdate(
-              { userName: ctx.from.username },
-              { cryptos }
-            );
-            ctx.reply(`${cur} deleted from favorite!`);
-          } else ctx.reply(`${cur} is not in favorite.`);
-        }
-      });
-  });
 }
 
-async function displayCryptoInfo(ctx, curObj) {
-  const userObj = await userModel.findOne({ userName: ctx.from.username });
-  ctx.reply(`name: ${curObj.name}\n${currencyString(curObj.quote)}`, {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: userObj.cryptos.includes(curObj.name)
-              ? "Remove from favorite"
-              : "Add to favorite",
-            callback_data: curObj.symbol,
-          },
-        ],
-      ],
-    },
-  });
+function sendAnswer(text, chat_id, res) {
+  axios
+    .post(`${telegram_url}sendMessage`, {
+      chat_id,
+      text,
+    })
+    .then((response) => res.status(200).send(response))
+    .catch((err) => res.send(err));
 }
 
-function currencyString(quoteObj) {
+function currencyString(curObj) {
   const outArr = [];
-  for (const [key, value] of Object.entries(quoteObj)) {
+  for (const [key, value] of Object.entries(curObj.quote.USD)) {
     if (typeof value === "string") {
       const str = `${key.replaceAll("_", " ")}: ${new Intl.DateTimeFormat(
         "en-US",
@@ -182,19 +232,11 @@ function currencyString(quoteObj) {
       outArr.push(str);
     }
   }
-  return outArr.join("\n");
+  return `name: ${curObj.name}\n${outArr.join("\n")}`;
 }
 
-async function followCurrency(userName, cryptoName) {
-  let { cryptos } = await userModel.findOne({ userName });
+main();
 
-  if (cryptos.includes(cryptoName))
-    cryptos.splice(cryptos.indexOf(cryptoName), 1);
-  else cryptos.push(cryptoName);
-
-  await userModel.findOneAndUpdate({ userName }, { cryptos });
-}
-
-app();
-
-bot.launch();
+app.listen(80, () => {
+  console.log("Server is running");
+});
--- src/cryptoSchema.js
+++ src/cryptoSchema.js
@@ -1,69 +1,7 @@
 import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
-  userName: String,
-  cryptos: [String],
+    userName: String,
+    cryptos: [String],
 });
-
 const userModel = mongoose.model("user", userSchema);
-
 export { userModel };
-
-// {
-//     id: 1,
-//     name: 'Bitcoin',
-//     symbol: 'BTC',
-//     slug: 'bitcoin',
-//     num_market_pairs: 8312,
-//     date_added: '2013-04-28T00:00:00.000Z',
-//     tags: [
-//       'mineable',
-//       'pow',
-//       'sha-256',
-//       'store-of-value',
-//       'state-channel',
-//       'coinbase-ventures-portfolio',
-//       'three-arrows-capital-portfolio',
-//       'polychain-capital-portfolio',
-//       'binance-labs-portfolio',
-//       'blockchain-capital-portfolio',
-//       'boostvc-portfolio',
-//       'cms-holdings-portfolio',
-//       'dcg-portfolio',
-//       'dragonfly-capital-portfolio',
-//       'electric-capital-portfolio',
-//       'fabric-ventures-portfolio',
-//       'framework-ventures-portfolio',
-//       'galaxy-digital-portfolio',
-//       'huobi-capital-portfolio',
-//       'alameda-research-portfolio',
-//       'a16z-portfolio',
-//       '1confirmation-portfolio',
-//       'winklevoss-capital-portfolio',
-//       'placeholder-ventures-portfolio',
-//       'pantera-capital-portfolio',
-//       'multicoin-capital-portfolio',
-//       'paradigm-portfolio'
-//     ],
-//     max_supply: 21000000,
-//     circulating_supply: 18888018,
-//     total_supply: 18888018,
-//     platform: null,
-//     cmc_rank: 1,
-//     last_updated: '2021-12-01T12:11:07.000Z',
-//     quote: { USD: [Object] }
-//   }
-
-// price: 47621.93509482286,
-// volume_24h: 320855523.3475463,
-// volume_change_24h: -16.0428,
-// percent_change_1h: 0.00177856,
-// percent_change_24h: -2.82409193,
-// percent_change_7d: -15.51152628,
-// percent_change_30d: -21.46368851,
-// percent_change_60d: -12.28717763,
-// percent_change_90d: -6.88479106,
-// market_cap: 12228824047.979734,
-// market_cap_dominance: 0.5603,
-// fully_diluted_market_cap: 12228824047.98,
-// last_updated: '2021-12-06T11:38:08.000Z'
